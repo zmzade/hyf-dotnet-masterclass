16ca858d9a5127d297c2299b8444dcb87180e2c6
zmzade <zmahmoodzadeh@yahoo.com>
2023-02-25 20:03:16 +0100

    add string reverse exercise

--- week1/Program.cs
+++ week1/Program.cs
@@ -1,6 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//homework week #1
+app.MapGet("/", () =>
+{
+    //1. string manipulation:reverse
+    string input = "world";
+    char[] chars = new char[input.Length];
+    for (int i = 0; i < input.Length; i++)
+    {
+        chars[i] = input[(input.Length - 1) - i];
+    }
+    // var chars = input.ToCharArray().Reverse();
+    return string.Join("", chars);
+
+
+
+});
 
 app.Run();
