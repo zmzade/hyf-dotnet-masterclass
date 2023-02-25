306bb751b8baaad4981d93d160d654ef9e185f94
zmzade <zmahmoodzadeh@yahoo.com>
2023-02-25 20:21:19 +0100

    add math array exercise

--- week1/Program.cs
+++ week1/Program.cs
@@ -2,9 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 //homework week #1
+//1. string manipulation:reverse
 app.MapGet("/", () =>
 {
-    //1. string manipulation:reverse
     string input = "world";
     char[] chars = new char[input.Length];
     for (int i = 0; i < input.Length; i++)
@@ -13,9 +13,25 @@ app.MapGet("/", () =>
     }
     // var chars = input.ToCharArray().Reverse();
     return string.Join("", chars);
-
-
-
 });
+
+//2. Math Array
+app.MapGet("/math", () =>
+{
+    int[] arr = new[] { 271, -3, 1, 14, -100, 13, 2, 1, -8, -59, -1852, 41, 5 };
+    var mathArray = new int[2];
+    var sum = 0;
+    var product = 1;
+    foreach (var number in arr)
+    {
+        if (number < 0) sum += number;
+        if (number > 1) product *= number;
+    }
+    mathArray[0] = sum;
+    mathArray[1] = product;
+    return mathArray;
+});
+
+
 
 app.Run();
