04390f32fc8ae1beab8e172af5fa8b58f047df80
zmzade <zmahmoodzadeh@yahoo.com>
2023-03-11 16:54:08 +0100

    add Account class

--- week3/Program.cs
+++ week3/Program.cs
@@ -8,10 +8,65 @@ app.MapGet("/interface", () =>
     var cow = new Cow();
     var animal = new Animal();
     return animal.MakeSound(cat);
-    return "hi";
+});
+
+app.MapGet("/account", () =>
+{
+    var account = new Account(200);
+    account.Withdraw(100);
+    account.Deposit(50);
+    return account.Balance;
 });
 
 app.Run();
+
+class Account
+{
+    public int Balance
+    {
+        get
+        {
+            int balance = 0;
+            foreach (var item in transactions)
+            {
+                balance += item.Amount;
+            }
+            return balance;
+        }
+    }
+    public Account(int amount)
+    {
+        Deposit(amount);
+    }
+
+    public List<Transaction> transactions = new List<Transaction>();
+    public void Withdraw(int amount)
+    {
+        if (amount <= 0)
+            throw new ArgumentOutOfRangeException(nameof(amount), "Withdraw should be positive");
+        if (Balance - amount < 0)
+            throw new InvalidOperationException("we should not be able to withdraw more than we have in the balance");
+        var transaction = new Transaction(-amount);
+        transactions.Add(transaction);
+    }
+
+    public void Deposit(int amount)
+    {
+        if (amount <= 0)
+            throw new ArgumentOutOfRangeException(nameof(amount), "Deposit should be positive");
+        var transaction = new Transaction(amount);
+        transactions.Add(transaction);
+    }
+}
+
+class Transaction
+{
+    public int Amount { get; }
+    public Transaction(int amount)
+    {
+        Amount = amount;
+    }
+}
 
 public interface IAnimal
 {
