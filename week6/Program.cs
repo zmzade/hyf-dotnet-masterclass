380020bca2805d9c9b68ae56db0beea96fb6742f
zmzade <zmahmoodzadeh@yahoo.com>
2023-04-09 18:50:37 +0200

    edit Program.cs

--- week6/Program.cs
+++ week6/Program.cs
@@ -3,59 +3,61 @@ using HackYourFuture.Week6;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<IRepository<User>, Repository<User>>();
 var app = builder.Build();
 
-
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/get", async (IRepository<User> repository) =>
-{
-    var table = "users";
-    return await repository.Get(table);
-});
 
 app.MapGet("/users", async (IUserRepository userRepository) =>
 {
-    return await userRepository.GetUsers();
+    return await userRepository.Get();
 });
 
-app.MapPost("/user", async(IUserRepository userRepository, User user)=>
+app.MapPost("/user", async (IUserRepository userRepository, User user) =>
 {
-    if(string.IsNullOrWhiteSpace(user.Fullname))
+    if (string.IsNullOrWhiteSpace(user.Fullname))
+    {
         throw new Exception("Fullname is required!");
-        if (string.IsNullOrWhiteSpace(user.Address))
+    }
+    if (string.IsNullOrWhiteSpace(user.Address))
+    {
         throw new Exception("Address is required!");
-    return await userRepository.PostUser(user);
+    }
+    return await userRepository.Create(user);
 });
 
-app.MapPut("/updateUser/{id}", async(IUserRepository userRepository, int id, User user)=>
+app.MapPut("/updateUser/{id}", async (IUserRepository userRepository, int id, User user) =>
 {
-    return await userRepository.UpdateUser(id, user);
+    return await userRepository.Update(id, user);
 });
 
-app.MapDelete("/deleteUser/{id}", async(int id, IUserRepository userRepository)=>
+app.MapDelete("/deleteUser/{id}", async (int id, IUserRepository userRepository) =>
 {
-    return await userRepository.DeleteUser(id);
+    return await userRepository.Delete(id);
 });
 
-app.MapGet("/product/{id}", async(int id, IProductRepository productRepository)=>
+app.MapGet("/product/{id}", async (int id, IProductRepository productRepository) =>
 {
     if (id < 0)
-    throw new Exception("Id should be a positive number");
+    {
+        throw new Exception("Id should be a positive number");
+    }
 
-    return await productRepository.GetProduct(id);
-    
+    return await productRepository.Get(id);
+
 });
 
-app.MapPost("/product", async(IProductRepository productRepository, Product product)=>
+app.MapPost("/product", async (IProductRepository productRepository, Product product) =>
 {
-    if(string.IsNullOrWhiteSpace(product.Name))
-    throw new Exception("Product's name is required!");
+    if (string.IsNullOrWhiteSpace(product.Name))
+    {
+        throw new Exception("Product's name is required!");
+    }
     if (product.Price is null)
-    throw new Exception("Product's price is required!");
-    return await productRepository.PostProduct(product);
+    {
+        throw new Exception("Product's price is required!");
+    }
+    return await productRepository.Create(product);
 });
-
 
 app.Run();
 
